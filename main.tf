3c71c4e89c1e738fe31f7e53f233dad5bd625e54
adrajvanshi <166098089+adrajvanshi@users.noreply.github.com>
2024-09-05 05:49:51 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,6 @@
+provider "aws" {
+region     = "us-east-1"
+}
 resource "aws_instance" "example" {
   ami           = "ami-02c21308fed24a8ab"  # Amazon Linux 2 AMI, change it as per your region
   instance_type = "t3.micro"  # Instance type
