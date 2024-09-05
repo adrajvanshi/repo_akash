988db6e5a16ee21ea548cd8b545877bf38b8f0b6
adrajvanshi <166098089+adrajvanshi@users.noreply.github.com>
2024-09-05 05:48:44 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
-
 resource "aws_instance" "example" {
   ami           = "ami-02c21308fed24a8ab"  # Amazon Linux 2 AMI, change it as per your region
   instance_type = "t3.micro"  # Instance type
 
   tags = {
- 
+    Name = "EC2 created through terraform by AkashR"
+  }
+}
